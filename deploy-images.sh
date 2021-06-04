bcfbf534169ede897b7d0a6bb650af01c46656e6
kornelro <kornel.ro@gmail.com>
2021-06-04 17:19:35 +0200

    deploy simulation images

--- deploy-images.sh
+++ deploy-images.sh
@@ -1,11 +1,25 @@
+HOST=kornelro
+
 docker build -t abm-0-data-preparation-activity-model 0-data-preparation/activity-model/.
-docker tag abm-0-data-preparation-activity-model kornelro/abm-0-data-preparation-activity-model:latest
-docker push kornelro/abm-0-data-preparation-activity-model:latest
+docker tag abm-0-data-preparation-activity-model $HOST/abm-0-data-preparation-activity-model:latest
+docker push $HOST/abm-0-data-preparation-activity-model:latest
 
 docker build -t abm-0-data-preparation-facilities 0-data-preparation/facilities/.
-docker tag abm-0-data-preparation-facilities kornelro/abm-0-data-preparation-facilities:latest
-docker push kornelro/abm-0-data-preparation-facilities:latest
+docker tag abm-0-data-preparation-facilities $HOST/abm-0-data-preparation-facilities:latest
+docker push $HOST/abm-0-data-preparation-facilities:latest
 
 docker build -t abm-0-data-preparation-network 0-data-preparation/network/.
-docker tag abm-0-data-preparation-network kornelro/abm-0-data-preparation-network:latest
-docker push kornelro/abm-0-data-preparation-network:latest
+docker tag abm-0-data-preparation-network $HOST/abm-0-data-preparation-network:latest
+docker push $HOST/abm-0-data-preparation-network:latest
+
+docker build -t abm-1-simulation-activity-model 1-simulation/activity-model/.
+docker tag abm-1-simulation-activity-model $HOST/abm-1-simulation-activity-model:latest
+docker push $HOST/abm-1-simulation-activity-model:latest
+
+docker build -t abm-1-simulation-population 1-simulation/population/.
+docker tag abm-1-simulation-population $HOST/abm-1-simulation-population:latest
+docker push $HOST/abm-1-simulation-population:latest
+
+docker build -t abm-1-simulation-traffic-model 1-simulation/traffic-model/.
+docker tag abm-1-simulation-traffic-model $HOST/abm-1-simulation-traffic-model:latest
+docker push $HOST/abm-1-simulation-traffic-model:latest
